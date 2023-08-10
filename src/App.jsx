1301e5f500f2b4a34ae12c0dd25952a374098e26
Jaco Antonio <jaco42818@gmail.com>
2023-08-10 12:05:32 -0300

    Alteração com intuito de resolver bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import {Route, Routes, BrowserRouter, Navigate} from 'react-router-dom'
-import { EditProfile, Home, Profile, Register, Photo, Search, Login } from './pages'
+import { EditProfile, Home, Profile, Register, Photo, Search } from './pages'
 import { Footer, Navbar } from './components'
+import { useAuth } from './hooks/useAuth'
 
 function App() {
-  const auth = true
+  const {loading, auth } = useAuth()
 
+  if (loading) {
+    return <div>loading...</div>
+  }
   return (
+    <div className="App">
       <BrowserRouter>
-        <div className="App">
-          <Navbar/>
-          <div className="container">
-          <Routes>
-            <Route path='/register' element={!auth ? <Register/> : <Navigate to='/'/>} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-          </div>
-          <Footer/>
-        </div>
+      <Navbar/>
+      <div className="container">
+      <Routes>
+        <Route path='/' element={auth ? <Home/> : <Navigate to='/login'/>} />
+        <Route path='/profile' element={auth ? <EditProfile/> : <Navigate to='/login'/>} />
+        <Route path='/users/:id' element={auth ? <Profile/> : <Navigate to='/login'/>} />
+        <Route path='/register' element={!auth ? <Register/> : <Navigate to='/'/>} />
+        <Route path='/login' element={!auth ? <Login/> : <Navigate to='/'/>} />
+        <Route path='/search' element={auth ? <Search/> : <Navigate to='/login'/>} />
+        <Route path='/photos/:id' element={auth ? <Photo/> : <Navigate to='/login'/>} />
+      </Routes>
+      </div>
+      <Footer/>
       </BrowserRouter>
+    </div>
   )
 }
 
