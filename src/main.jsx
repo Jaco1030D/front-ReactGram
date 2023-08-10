83f180c8230d66d0f60d0499203c6332a0fa85ac
Jaco Antonio <jaco42818@gmail.com>
2023-08-10 08:29:03 -0300

    Alteração com intuito de resolver bug

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
     <App />
     </Provider>
   </React.StrictMode>,
