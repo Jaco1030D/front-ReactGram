3f63f15420fb302aa0d5a43de8ca994cf854ede0
Jaco Antonio <jaco42818@gmail.com>
2023-08-10 08:16:12 -0300

    Alteração com intuito de resolver bug

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 import { Message } from '../../components'
-import { login, reset } from '../../slices/AuthSlices'
+// import { login, reset } from '../../slices/AuthSlices'
 import './Auth.css'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  const {loading, error} = useSelector((state) => state.auth)
+  // const {loading, error} = useSelector((state) => state.auth)
 
   const handleSubmit = (e) =>{
     e.preventDefault()
@@ -21,19 +21,19 @@ const Login = () => {
     }
     console.log(user)
 
-    dispatch(login(user))
+  //   dispatch(login(user))
   }
-  useEffect(() => {
-    dispatch(reset())
-  },[dispatch])
+  // useEffect(() => {
+  //   dispatch(reset())
+  // },[dispatch])
   return (
     <div id='register'>
       <h2>Entrar</h2>
       <form onSubmit={handleSubmit} >
         <input type="email" placeholder='Digite seu E-mail...' value={email} onChange={(e) => setEmail(e.target.value)}/>
         <input type="password" placeholder='Digite a senha...' value={password} onChange={(e) => setPassword(e.target.value)}/>
-        {loading && <input type="submit" value="Aguarde..." disabled />}
-        {!loading && <input type="submit" value="Entrar"  />}
+        {/* {loading && <input type="submit" value="Aguarde..." disabled />} */}
+        <input type="submit" value="Entrar"  />
         {error && <Message msg={error} type="error" />}
       </form>
       <p>
